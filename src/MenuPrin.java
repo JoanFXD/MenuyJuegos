ce966fd15af803bfe0a7c92ca7e0dcc9fc3d25c3
JoanFXD <jsfarfans@udistrital.edu.co>
2025-11-07 13:24:56 -0500

    Base del menu

--- src/MenuPrin.java
+++ src/MenuPrin.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class MenuPrin {
+    private JButton triquibutton;
+    private JButton calculadoraButton;
+    private JButton parejasButton;
+    private JButton ahorcadoButton;
+    private JLabel tituloprin;
+    private JButton salir;
+    public void configBotonIcon(){
+        ImageIcon iconoTriqui = new ImageIcon(getClass().getResource("resources/imagenes/unnamed.png"));
+        Image imgTriqui = iconoTriqui.getImage().getScaledInstance(24, 24, Image.SCALE_SMOOTH);
+        triquibutton.setIcon(new ImageIcon(imgTriqui));
+    }
 }
+
