560eaff1d167a75381e881826ef2c7e3e40d436c
Arter-code <jdrianov@udistrital.edu.co>
2025-11-07 13:34:38 -0500

    Triqui incompleto

---
+++ src/Triqui.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Triqui {
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton[] botones;
+    private String[][] tablero;
+
+    public Triqui() {
+        tablero = new String[3][3];
+        botones = new JButton[]{button1,button2,button3,button4,button5,button6,button7,button8,button9};
+        for (int i=0; i<botones.length;i++){
+
+        }
+
+
+    }
+
+
+}
