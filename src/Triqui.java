b27f8f981ecd5a59b98147e7a6c9dd301d8c198d
Arter-code <jdrianov@udistrital.edu.co>
2025-11-10 17:20:47 -0500

    Ahora los botones marcan X y O (Creo)

--- src/Triqui.java
+++ src/Triqui.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
 public class Triqui {
     private JButton button1;
     private JButton button2;
@@ -12,18 +11,39 @@ public class Triqui {
     private JButton button7;
     private JButton button8;
     private JButton button9;
-    private JButton[] botones;
+    private JButton[][] botones;
     private String[][] tablero;
+    private int turn=1;
 
     public Triqui() {
         tablero = new String[3][3];
-        botones = new JButton[]{button1,button2,button3,button4,button5,button6,button7,button8,button9};
+        botones = new JButton[][]{{button1,button2,button3},{button4,button5,button6},{button7,button8,button9}};
         for (int i=0; i<botones.length;i++){
+            for (int c=0; c< botones[i].length;c++){
+                final int fila =i;
+                final int columna=c;
+                botones[i][c].addActionListener(e -> {
+                    try {
+                        if(turn%2 !=0){
+                            tablero[fila][columna]= "X";
+                            botones[fila][columna].setText("X");
+                        } else {
+                            tablero[fila][columna]="O";
+                            botones[fila][columna].setText("O");
+                        }
+
+                    } catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(
+                                null,
+                                "Ingrese números válidos",
+                                "Error",
+                                JOptionPane.ERROR_MESSAGE
+                        );
+                    }
+                    turn++;
+                });
+            }
 
         }
-
-
     }
-
-
 }
